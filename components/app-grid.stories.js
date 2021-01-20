e788ca1ab5beee679a10efd9e257ff8fc7168dcd
Joao Duarte <joaopd17@hotmail.com>
2021-01-20 17:48:22 +0100

    add stories files to grid nd nav

---
+++ components/app-grid.stories.js
@@ -0,0 +1,10 @@
+import '../styles/index.css'
+
+import './app-grid.css'
+import grid from './app-grid.html'
+
+export default {
+  title: 'grid',
+}
+
+export const base = () => grid
---
+++ components/navigation.stories.js
@@ -0,0 +1,10 @@
+import '../styles/index.css'
+
+import './app-nav.css'
+import navigation from './navigation.html'
+
+export default {
+  title: 'navigation',
+}
+
+export const base = () => navigation
