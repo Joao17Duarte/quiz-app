ffdd0a13b7796466215dbc5d54f09bbb48f85f8e
Joao Duarte <joaopd17@hotmail.com>
2021-02-01 16:53:04 +0100

    refactor navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,66 +1,23 @@
-import getByDataJs from './getByDataJs'
+import getAllDataJs from './getAllDataJs'
 
 export default function navigation() {
-  const home = getByDataJs('section-home'),
-    bookmark = getByDataJs('section-bookmark'),
-    form = getByDataJs('section-create'),
-    settings = getByDataJs('section-settings'),
-    navHome = getByDataJs('btnHome'),
-    navBook = getByDataJs('btnBook'),
-    navCreate = getByDataJs('btnCreate'),
-    navSetting = getByDataJs('btnSettings'),
-    title = document.querySelector('h1')
+  const pages = getAllDataJs('page')
+  const navButtons = getAllDataJs('navBtn')
 
-  navHome.addEventListener('click', () => {
-    home.classList.remove('hidden')
-    bookmark.classList.add('hidden')
-    form.classList.add('hidden')
-    settings.classList.add('hidden')
+  navButtons.forEach(button => {
+    const clickedButtonName = button.dataset.name
+    button.addEventListener('click', () => {
+      pages.forEach(page => {
+        const pageName = page.dataset.name
+        page.classList.toggle('hidden', clickedButtonName !== pageName)
+      })
 
-    title.textContent = 'QuizGenerator'
-
-    navHome.classList.add('active')
-    navBook.classList.remove('active')
-    navCreate.classList.remove('active')
-    navSetting.classList.remove('active')
-  })
-
-  navBook.addEventListener('click', () => {
-    home.classList.add('hidden')
-    bookmark.classList.remove('hidden')
-    form.classList.add('hidden')
-    settings.classList.add('hidden')
-    title.textContent = 'Bookmark'
-
-    navHome.classList.remove('active')
-    navBook.classList.add('active')
-    navCreate.classList.remove('active')
-    navSetting.classList.remove('active')
-  })
-
-  navCreate.addEventListener('click', () => {
-    home.classList.add('hidden')
-    bookmark.classList.add('hidden')
-    form.classList.remove('hidden')
-    settings.classList.add('hidden')
-    title.textContent = 'Create your Question'
-
-    navHome.classList.remove('active')
-    navBook.classList.remove('active')
-    navCreate.classList.add('active')
-    navSetting.classList.remove('active')
-  })
-
-  navSetting.addEventListener('click', () => {
-    home.classList.add('hidden')
-    bookmark.classList.add('hidden')
-    form.classList.add('hidden')
-    settings.classList.remove('hidden')
-    title.textContent = 'Settings'
-
-    navHome.classList.remove('active')
-    navBook.classList.remove('active')
-    navCreate.classList.remove('active')
-    navSetting.classList.add('active')
+      navButtons.forEach(button => {
+        button.classList.toggle(
+          'active',
+          clickedButtonName === button.dataset.name
+        )
+      })
+    })
   })
 }
